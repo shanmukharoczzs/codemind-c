fe3890a4883260aff8ab83bc012137419ce6ba81
shanmukharoczzs <97242139+shanmukharoczzs@users.noreply.github.com>
2023-10-11 21:20:28 +0530

    #include <stdio.h>
    int main() {
        int i,j,sum=0,n;
        scanf("%d",&n);
        
       
        for(i=1;i<=n;i++){
           sum+=i;
            
        }
        printf("%d",sum);
        
           
        return 0;
    }

---
+++ Sum_of_first_N_natural_Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,sum=0,n;
+    scanf("%d",&n);
+    
+   
+    for(i=1;i<=n;i++){
+       sum+=i;
+        
+    }
+    printf("%d",sum);
+    
+       
+    return 0;
+}
